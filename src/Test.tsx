cd3dccaf0458669cd4c0d8d5259114c5f63f7158
KrisandyR <krisandyriver@gmail.com>
2023-02-06 16:00:07 +0700

    First Commit

---
+++ src/Test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Test() {
+  return (
+    <div className="App">
+        <h1>Training IT Div</h1>
+    </div>
+  );
+}
+
+export default Test;
